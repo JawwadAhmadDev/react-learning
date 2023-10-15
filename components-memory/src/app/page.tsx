7c459134d6fe869ff798e6372726e97976ce666b
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-10-15 15:34:37 +0500

    4th challenge solved

--- components-memory/src/app/page.tsx
+++ components-memory/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Gallery from '@/components/Gallery'
 import Form from '@/components/Form'
-import FeedbackForm from '@/components/FeedbackForm'
+// import FeedbackForm from '@/components/FeedbackForm'
+import FeedbackForm from '@/components/FeedbackForm2'
 
 export default function Home() {
   return (
     // <Gallery />
     // <Form/>
-    <FeedbackForm/>
+    // <FeedbackForm/>
+    <FeedbackForm />
   )
 }
